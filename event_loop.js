872ca4553a53e6cb5b5cb7037dc5543f5705698a
Md Ashraf Hussain <ashrafhussain.2317.csedu@gmail.com>
2021-03-21 11:37:48 +0600

    producer and consumer service added

--- event_loop.js
+++ event_loop.js
@@ -1,7 +1,12 @@
 const fs = require('fs');
+// setImmediate goes to queue for printing log
 setImmediate(() => console.log(1));
+// promise resolve also goes to queue for printing
 Promise.resolve().then(() => console.log(2));
+// nextTick() runs next, adding callback 3 to the next tick
+// microtask queue. 
 process.nextTick(() => console.log(3));
+
 fs.readFile(__filename, () => {
   console.log(4);
   setTimeout(() => console.log(5));
